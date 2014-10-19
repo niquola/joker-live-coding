c85296a7aefde80a4e9ec2ec5c2db72f3650f422
niquola <niquola@gmail.com>
2014-10-20 00:41:19 +0400

    prepare app.js

--- resources/public/app.js
+++ resources/public/app.js
@@ -1,1 +1,24 @@
-alert("hi")
+alert('here')
+
+var l = window.location
+var url = 'ws://' + l.host + '/chat' + l.pathname
+var ws = new WebSocket(url)
+
+  function byId(id){
+    return document.getElementById(id)
+  }
+
+var out = byId('out')
+var inp = byId('inp')
+
+inp.onkeyup = function(e) {
+  if (e.ctrlKey && e.keyCode == 13){
+    ws.send(inp.value)
+  }
+}
+
+ws.onmessage = function(e) {
+  console.log(e.data)
+  out.innerHTML += e.data
+}
+
